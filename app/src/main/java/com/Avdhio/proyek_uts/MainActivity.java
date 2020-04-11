62b3cba094270f9d763d29b459d3d8229e948abc
AvdhioBayu <avdhiob@gmail.com>
2020-04-11 09:10:46 +0700

    Merapikan tampilan

--- app/src/main/java/com/Avdhio/proyek_uts/MainActivity.java
+++ app/src/main/java/com/Avdhio/proyek_uts/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 
 import com.Avdhio.proyek_uts.Adapter.GameAdapter;
@@ -13,7 +14,7 @@ import com.Avdhio.proyek_uts.Model.GameLogo;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements GameAdapter.OnitemListener {
+public class MainActivity extends AppCompatActivity {
     private RecyclerView rvPc;
     private ArrayList<GameLogo>list = new ArrayList<>();
 
@@ -21,29 +22,13 @@ public class MainActivity extends AppCompatActivity implements GameAdapter.Onite
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        rvPc = findViewById(R.id.rv_pc);
-        list.addAll(DataDummy.GetListData());
-        rvPc.setLayoutManager(new LinearLayoutManager(this));
-        GameAdapter gameAdapter = new GameAdapter(list,this);
-        rvPc.setAdapter(gameAdapter);
-    }
-
-    @Override
-    public void onListClicked(int index, GameLogo item) {
-        String detail = item.getDetail();
-        String spec = item.getSpec();
-        String nama = item.getName();
-        int logogame = item.getLogogame();
-        Intent intent = new Intent(this, DetailActivity.class);
-        intent.putExtra("Nama Game", nama);
-        intent.putExtra("Logo Game", logogame);
-        intent.putExtra("Data Detail",detail);
-        intent.putExtra("Data Spec" , spec);
-        startActivity(intent);
-    }
-
-    @Override
-    public void onPointerCaptureChanged(boolean hasCapture) {
-
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, PcActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 1000);
     }
 }
